4b1d562b22c780622582e19ad7c6571f6715388a
Cool Dev <amacool0117@outlook.com>
2019-09-30 11:50:00 +0300

    add addresses page

--- src/react/components/Addresses/index.js
+++ src/react/components/Addresses/index.js
@@ -4,6 +4,7 @@ import InputLabel from "@material-ui/core/InputLabel/InputLabel";
 import Select from "@material-ui/core/Select/Select";
 import MenuItem from "@material-ui/core/MenuItem/MenuItem";
 import FormControl from "@material-ui/core/FormControl/FormControl";
+import TextField from "@material-ui/core/TextField/TextField";
 import { CustomTable } from "../common/CustomTable";
 import { CustomInput } from "../common/CustomInput";
 import { CustomCheckbox } from "../common/CustomCheckbox";
@@ -26,7 +27,6 @@ function Addresses({ settings, handleSetValue }) {
     notes: ''
   });
   const [addresses, setAddresses] = React.useState([]);
-  const language = 'EN';
 
   React.useEffect(() => {
     setCategoryLabelWidth(categoryLabel.current && categoryLabel.current.offsetWidth);
@@ -41,13 +41,12 @@ function Addresses({ settings, handleSetValue }) {
   };
 
   const handleAdd = () => {
-    if (state.address === ''
-      || state.street === ''
+    console.log(state);
+    if (state.street === ''
       || state.city === ''
       || state.province === ''
       || state.zipCode === ''
       || state.country === ''
-      || state.location === ''
       || state.region === ''
       || state.latitude === ''
       || state.longitude === ''
@@ -65,111 +64,150 @@ function Addresses({ settings, handleSetValue }) {
         <h5>addresses</h5>
       </div>
       <div className="content content-header">
-        <div className="row inline">
-          <div className="col">
-            <CustomCheckbox
-              label="address"
-              onChange={handleChange('address')}
-              required={true}
-              value={state.address}
-            />
+        <div className="row">
+          <div className="inline mb-20">
+            <div className="col w-31 mr-15">
+              <CustomCheckbox
+                label="address"
+                onChange={handleChange('address')}
+                required={true}
+                value={state.address}
+              />
+            </div>
+            <div className="col">
+              <CustomInput
+                id="street"
+                label="Street"
+                required={true}
+                onChange={handleChange("street")}
+              />
+            </div>
           </div>
-          <div className="col">
-            <CustomInput
-              id="street"
-              label="Street"
-              required={true}
-              onChange={handleChange("street")}
-            />
-          </div>
-        </div>
 
-        <div className="row inline">
-          <div className="col">
-            <CustomInput
-              id="city"
-              label="City"
-              required={true}
-              onChange={handleChange("city")}
-            />
+          <div className="inline mb-20">
+            <div className="col mr-15">
+              <CustomInput
+                id="city"
+                label="City"
+                required={true}
+                onChange={handleChange("city")}
+              />
+            </div>
+            <div className="col mr-15">
+              <CustomInput
+                id="province"
+                label="Province"
+                required={true}
+                onChange={handleChange("province")}
+              />
+            </div>
+            <div className="col mr-15">
+              <CustomInput
+                id="zip-code"
+                label="Zip Code"
+                required={true}
+                onChange={handleChange("zipCode")}
+              />
+            </div>
+            <div className="col mr-15 mt-26">
+              <FormControl variant="outlined" className="form-control custom-outlined-form-control">
+                <InputLabel ref={categoryLabel} htmlFor="country" className="custom-select-label">
+                  Country<b>*</b>
+                </InputLabel>
+                <Select
+                  value={state.country}
+                  onChange={handleChange('country')}
+                  labelWidth={categoryLabelWidth}
+                  inputProps={{
+                    name: 'country',
+                    id: 'country',
+                  }}
+                  className="custom-select"
+                  placeholder="Primary"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value="EN">
+                    <em>England</em>
+                  </MenuItem>
+                </Select>
+              </FormControl>
+            </div>
           </div>
-          <div className="col">
-            <CustomInput
-              id="province"
-              label="Province"
-              required={true}
-              onChange={handleChange("province")}
-            />
+
+          <div className="inline mb-20">
+            <div className="col mr-15">
+              <CustomCheckbox
+                label="Location"
+                onChange={handleChange('location')}
+                required={true}
+                value={state.location}
+              />
+            </div>
+            <div className="col mr-15">
+              <CustomInput
+                id="region"
+                label="Region"
+                required={true}
+                onChange={handleChange("region")}
+              />
+            </div>
+            <div className="col mr-15">
+              <CustomInput
+                id="latitude"
+                label="Latitude"
+                required={true}
+                onChange={handleChange("latitude")}
+              />
+            </div>
+            <div className="col mr-15">
+              <CustomInput
+                id="longitude"
+                label="Longitude"
+                required={true}
+                onChange={handleChange("longitude")}
+              />
+            </div>
           </div>
-          <div className="col">
-            <CustomInput
-              id="zip-code"
-              label="Zip Code"
-              required={true}
-              onChange={handleChange("zipCode")}
-            />
-          </div>
-          <div className="col">
-            <FormControl variant="outlined" className="form-control custom-outlined-form-control">
-              <InputLabel ref={categoryLabel} htmlFor="country" className="custom-select-label">
-                Country<b>*</b>
-              </InputLabel>
-              <Select
-                value={state.country}
-                onChange={handleChange('country')}
-                labelWidth={categoryLabelWidth}
-                inputProps={{
-                  name: 'country',
-                  id: 'country',
-                }}
-                className="custom-select"
-                placeholder="Primary"
+
+          <div className="inline mb-20">
+            <div className="w-69 mr-15 mt-26">
+              <TextField
+                value={state.notes}
+                id="notes"
+                label="Notes"
+                multiline
+                rows="5"
+                onChange={handleChange('notes')}
+                className="text-field custom-textarea-control"
+                variant="outlined"
+                placeholder="Notes"
+              />
+            </div>
+            <div className="w-34 flex-end">
+              <Button
+                variant="contained"
+                className="add-button"
+                onClick={handleAdd}
               >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value="EN">
-                  <em>England</em>
-                </MenuItem>
-              </Select>
-            </FormControl>
+                ADD
+              </Button>
+            </div>
           </div>
         </div>
-
-        <div className="row inline">
-          <div className="col">
-            <CustomCheckbox
-              label="Location"
-              onChange={handleChange('location')}
-              required={true}
-              value={state.location}
-            />
-          </div>
-          <div className="col">
-            <CustomInput
-              id="region"
-              label="Region"
-              required={true}
-              onChange={handleChange("region")}
-            />
-          </div>
-          <div className="col">
-            <CustomInput
-              id="doc-number"
-              label="Document Number"
-              required={true}
-              onChange={handleChange("docNumber")}
-            />
-          </div>
-          <div className="col">
-            <CustomInput
-              id="latitude"
-              label="Latitude"
-              required={true}
-              onChange={handleChange("latitude")}
-            />
-          </div>
-        </div>
+      </div>
+      <div className="content content-body">
+        <CustomTable
+          header={['Address', 'Location', 'Notes']}
+          data={addresses.map((item) =>
+            ({
+              a: `${item.street} ${item.city} ${item.province} ${item.zipCode} ${item.country}`,
+              b: item.region,
+              c: item.notes
+            })
+          )}
+        />
       </div>
     </div>
   )
