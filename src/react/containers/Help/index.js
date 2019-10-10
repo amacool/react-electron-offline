7b77fc2f04f4a595697d5a778a9191f32ca1f15f
Cool Dev <amacool0117@outlook.com>
2019-10-11 00:58:05 +0300

    load & show help text from mock

--- src/react/containers/Help/index.js
+++ src/react/containers/Help/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import axios from "axios";
+import "./styles.css";
 
 function Recent() {
+  const [content, setContent] = React.useState({});
+  const lang = "EN";
+
+  React.useEffect(() => {
+    axios.get('/data/help.json')
+      .then(function (result) {
+        setContent(result.data.content[0]);
+      });
+  }, []);
 
   return (
     <div className="Help">
@@ -13,50 +23,14 @@ function Recent() {
         </div>
         <div className="content-body">
           <ul>
-            <li>
-              <div>Membership and Election</div>
-              <div>How are the non-permanent members selected?</div>
-            </li>
-            <li>
-              <div>Conduct of Business</div>
-              <div>How is the work of the Security Council organized?</div>
-              <div>What is the difference between open and closed meetings and consultations?</div>
-              <div>What are subsidiary organs?</div>
-              <div>What is the difference between peacekeeping operations, political missions and peacebuilding offices?</div>
-            </li>
-            <li>
-              <div>Action with Respect to Threats to the Peace, Breach of the Peace, or Act of Aggression</div>
-              <div>How does the Security Council determine the existence of any threat to the peace, breach of the peace,
-                or act of aggression?</div>
-              <div>What are sanctions?</div>
-              <div>What kind of measures involving the use of armed force has the Security Council imposed in the past?</div>
-              <div>What are the rules for the use of force by States?</div>
-            </li>
-            <li>
-              <div>Admission of New Members to the United Nations</div>
-              <div>What is the Security Council's role in the admission of new members to the UN?</div>
-            </li>
-            <li>
-              <div>Security Council Reform</div>
-              <div>What is the process for Security Council reform?</div>
-            </li>
-            <li>
-              <div>About the Repertoire</div>
-              <div>Where does the material in the Repertoire come from</div>
-              <div>What is a case study?</div>
-              <div>What are implicit and explicit references?</div>
-              <div>What does the double asterisk (**) that is next to some headings mean?</div>
-              <div>Where is the veto featured in the Repertoire?</div>
-              <div>Where can I find information regarding the Security Council's activities in connection to the situation
-              in a specific country or on a specific topic?</div>
-            </li>
-            <li className="non-list-style">
-              <div>Other Questions</div>
-              <div>How is the Secretary-General elected?</div>
-            </li>
-            <li className="non-list-style">
-              <div>Other Resources</div>
-            </li>
+            {content[lang] && content[lang].map((item, index1) => (
+              <li key={index1}>
+                <div>{item['bulleted-list']}</div>
+                {item['h-link'].map((subItem, index2) => (
+                  <div key={index2}><a href={subItem.link.indexOf('https') >= 0 ? subItem.link : `https://${subItem.link}`}>{subItem.text}</a></div>
+                ))}
+              </li>
+            ))}
           </ul>
         </div>
       </div>
