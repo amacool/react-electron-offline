d5e9434dfdf5118519a88364ec1d328d22052351
Cool Dev <amacool0117@outlook.com>
2019-11-05 12:54:12 +0200

    updat display case for addresses

--- src/react/components/Addresses/index.js
+++ src/react/components/Addresses/index.js
@@ -326,14 +326,14 @@ function Addresses({ settings, handleSetValue, data, vocabularies, validating })
       <div className="content content-body">
         <CustomTable
           header={[
-            vocabularies[lang]['new']['addresses'][0],
-            vocabularies[lang]['new']['addresses'][6],
+            vocabularies[lang]['new']['common'][4],
+            vocabularies[lang]['new']['features'][0],
             vocabularies[lang]['new']['common'][3]
           ]}
           data={addresses.map((item) =>
             ({
-              a: `${item.street} ${item.city} ${item.province} ${item.zipCode} ${item.country}`,
-              b: `${item.region} ${item.latitude} ${item.longitude}`,
+              a: item.address ? vocabularies[lang]['new']['addresses'][0] : vocabularies[lang]['new']['addresses'][6],
+              b: item.address ? `${item.street} ${item.city} ${item.province} ${item.zipCode} ${item.country}` : `${item.region} ${item.latitude} ${item.longitude}`,
               c: item.notes
             })
           )}
